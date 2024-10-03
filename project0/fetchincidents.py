d96624e504d13fd7d00d7a8bd56e0fb3a36da614
Ishtmeet Singh Arora <isarora11@icloud.com>
2024-10-02 22:30:09 -0400

    test case passed

--- project0/fetchincidents.py
+++ project0/fetchincidents.py
@@ -3,5 +3,5 @@ import urllib.request
 def fetchIncidents(url):
     headers ={}
     headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-    data = urllib.request.urlopen(urllib.request.Request(url, headers=headers)).read()
-    return data
+    info = urllib.request.urlopen(urllib.request.Request(url, headers=headers)).read()
+    return info
